706bca0096a2faef38c752d82b457bce0b67bd4e
timp <thonghitak.tim@gmail.com>
2022-06-23 11:06:44 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Thongphitak Sowanna on 23/6/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/iOS_Simple_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Simple_AppApp.swift
+//  Shared
+//
+//  Created by Thongphitak Sowanna on 23/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Simple_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
